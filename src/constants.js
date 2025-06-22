9e10212e28d61db2b42b883979b390baaef07938
normanwashere <normanarisdeocareza@gmail.com>
2025-06-22 18:03:42 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
 
+// src/constants.js
+// This file could optionally hold constants, but for now, main.js has core constants.
+// We can move constants like default session duration here later if needed.
