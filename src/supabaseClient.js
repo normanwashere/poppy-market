0e9840d4f21c072d5f6227b3832f718bc3a36ff6
normanwashere <normanarisdeocareza@gmail.com>
2025-06-22 18:03:28 +0800

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,17 @@
+// src/supabaseClient.js
+const SUPABASE_URL = 'https://lmzxjxumfqjrvcnsrfbr.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtenhqeHVtZnFqcnZjbnNyZmJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MDUzNDIsImV4cCI6MjA2NjA4MTM0Mn0.7vd1mZzXKYjf64b_lRmPUR0KM_4VdgJMxsDVrCEYbTw';
+export const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export const state = {
+    isLoggedIn: false,
+    currentUser: null,
+    profile: null,
+    currentPage: 'login',
+    ruleToDelete: null,
+    activeLiveSession: null,
+    currentRuleSetId: null,
+    globalSettings: {},
+};
+
+export let channels = []; // Export channels for management in main.js
